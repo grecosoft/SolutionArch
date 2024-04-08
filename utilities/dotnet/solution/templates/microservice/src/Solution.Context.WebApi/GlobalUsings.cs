0dd9d62f2df89c3b0d9d58f2139de1ece4eefc9e
Brian Greco <blgreco72dev@gmail.com>
2024-04-08 17:02:18 -0400

    Added microservice-solution dotnet templates

---
+++ utilities/dotnet/solution/templates/microservice/src/Solution.Context.WebApi/GlobalUsings.cs
@@ -0,0 +1,10 @@
+// Global using directives
+
+global using System.Diagnostics;
+global using Destructurama;
+global using NetFusion.Core.Bootstrap.Container;
+global using NetFusion.Services.Serilog;
+global using NetFusion.Web.Extensions;
+global using Serilog;
+global using Serilog.Events;
+global using Serilog.Sinks.SystemConsole.Themes;
