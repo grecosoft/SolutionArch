18c8f6489850548170e45b339b64093355c62bfa
Brian Greco <blgreco72dev@gmail.com>
2024-04-12 17:12:33 -0400

    Uncommented statements

--- infrastructure/aks/deploy-custer.sh
+++ infrastructure/aks/deploy-custer.sh
@@ -3,11 +3,11 @@
 export TF_VAR_storage_resource_group_name="Terraform"
 export TF_VAR_state_storage_account_name="terraformstate06410"
 
-# terraform -chdir=cluster/terraform init \
-#   -backend-config="resource_group_name=${TF_VAR_storage_resource_group_name}" \
-#   -backend-config="storage_account_name=${TF_VAR_state_storage_account_name}"
+terraform -chdir=cluster/terraform init \
+  -backend-config="resource_group_name=${TF_VAR_storage_resource_group_name}" \
+  -backend-config="storage_account_name=${TF_VAR_state_storage_account_name}"
 
-# terraform -chdir=cluster/terraform apply -auto-approve
+terraform -chdir=cluster/terraform apply -auto-approve
 
 terraform -chdir=gateway/terraform init \
   -backend-config="resource_group_name=${TF_VAR_storage_resource_group_name}" \
